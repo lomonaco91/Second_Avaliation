889500d042a2ca0b5b8ec9c2410a9457fe4c370b
Aluno0313 <Aluno0313@univas.edu.br>
2016-06-14 22:06:47 -0300

    5 change

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+//CRIANDO TABELAS APOS CLICAR ADD
+var createData = {
+
+  //Inserindo Linhas
+  insertLine: function(){
+    var table = document.getElementById("collection").innerHTML
+
+    //Inserindo Linhas - onde irá ficar os dados dos PRODUTOS
+    var row = collection.insertRow(numberRows);
+  }
+
+var data = document.getElementById('collection');
+
+var row = collection.insertRow(1);
+row.innerHTML = ""+name+" "+price+";
+};
